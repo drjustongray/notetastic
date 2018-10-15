58d8078d57e4405aae1644147a6e9b34d0c57ccb
Justin <drjustongray@gmail.com>
2018-10-15 00:13:51 -0500

    setup middleware and services

--- NotetasticApi/Startup.cs
+++ NotetasticApi/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,38 +12,69 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using NotetasticApi.Common;
+using NotetasticApi.Users;
 
 namespace NotetasticApi
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var jwtSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["jwt:key"]));
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateAudience = false,
+						ValidateIssuer = false,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = jwtSigningKey,
+						ClockSkew = TimeSpan.FromSeconds(30)
+					};
+				});
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+			services.AddSingleton<IUserService, UserService>();
+			services.AddSingleton<IRefreshTokenRepository, RefreshTokenRepository>();
+			services.AddSingleton<IUserRepository, UserRepository>();
+			services.AddSingleton<ITokenService, TokenService>();
+			services.AddSingleton<IValidationService, ValidationService>();
+			services.AddSingleton<IPasswordService, PasswordService>();
+			var mongoClient = new MongoClient(Configuration["mongo:connection_string"]);
+			var db = mongoClient.GetDatabase("notetasticAPI");
+			services.AddSingleton<IMongoCollection<User>>(db.GetCollection<User>("Users"));
+			services.AddSingleton<IMongoCollection<RefreshToken>>(db.GetCollection<RefreshToken>("RefreshTokens"));
+			services.AddSingleton<SecurityKey>(jwtSigningKey);
+		}
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseAuthentication();
+			app.UseMvc();
+		}
+	}
 }
