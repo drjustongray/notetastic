373831c2db5774f37a0cadf3d6be0b5ea9a183a4
Justin <drjustongray@gmail.com>
2018-11-09 23:34:40 -0600

    basic auth form

---
+++ webclient/src/auth/Form.stories.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Form, { AuthFunction } from "./Form"
+
+const stories = storiesOf("AuthForm", module)
+
+const successfulLogin: AuthFunction = (username, password, rememberMe) => {
+	action("login")(username, password, rememberMe)
+	return Promise.resolve()
+}
+
+const failedLogin: AuthFunction = (username, password, rememberMe) => {
+	action("login")(username, password, rememberMe)
+	return Promise.reject({ message: "Sad" })
+}
+
+const successfulRegister: AuthFunction = (username, password, rememberMe) => {
+	action("register")(username, password, rememberMe)
+	return Promise.resolve()
+}
+
+const failedRegister1: AuthFunction = (username, password, rememberMe) => {
+	action("register")(username, password, rememberMe)
+	return Promise.reject({ message: "Sad" })
+}
+
+const failedRegister2: AuthFunction = (username, password, rememberMe) => {
+	action("register")(username, password, rememberMe)
+	return Promise.reject({ username: "bad username! bad!" })
+}
+
+const failedRegister3: AuthFunction = (username, password, rememberMe) => {
+	action("register")(username, password, rememberMe)
+	return Promise.reject({ password: "Bad password" })
+}
+
+const failedRegister4: AuthFunction = (username, password, rememberMe) => {
+	action("register")(username, password, rememberMe)
+	return Promise.reject({ message: "Sad", username: "bad username! bad!", password: "Bad password" })
+}
+
+stories.add(
+	"With Successful Login",
+	() => <Form action={successfulLogin} title="Login" />
+)
+
+stories.add(
+	"With Failed Login",
+	() => <Form action={failedLogin} title="Login" />
+)
+stories.add(
+	"With Successful Register",
+	() => <Form action={successfulRegister} title="Register" />
+)
+
+stories.add(
+	"With Failed Register (message)",
+	() => <Form action={failedRegister1} title="Register" />
+)
+
+stories.add(
+	"With Failed Register (username)",
+	() => <Form action={failedRegister2} title="Register" />
+)
+
+stories.add(
+	"With Failed Register (password)",
+	() => <Form action={failedRegister3} title="Register" />
+)
+
+stories.add(
+	"With Failed Register (all)",
+	() => <Form action={failedRegister4} title="Register" />
+)
---
+++ webclient/src/auth/Form.tsx
@@ -0,0 +1,61 @@
+import * as React from "react"
+import { Formik, Field, Form, FormikActions, FormikProps, ErrorMessage } from "formik"
+
+export type AuthFunction = (username: string, password: string, rememberMe: boolean) => Promise<void>
+
+
+export interface FormProps {
+	action: AuthFunction
+	title: string
+}
+
+interface FormValues {
+	username: string
+	password: string
+	rememberMe: boolean
+}
+
+const onSubmit = async (action: AuthFunction, values: FormValues, actions: FormikActions<FormValues>) => {
+	try {
+		await action(values.username, values.password, values.rememberMe)
+		actions.setSubmitting(false)
+	} catch (e) {
+		actions.setSubmitting(false)
+		actions.setErrors(e)
+		actions.setStatus(e)
+	}
+}
+
+const makeLabel = (forWhat: string, text: string) => <label htmlFor={forWhat}>{text}</label>
+
+const render = (title: string, { status, isSubmitting }: FormikProps<FormValues>) => (
+	<Form>
+		<h2>{title}</h2>
+		<div>
+			{makeLabel("auth-form-username", "Username")}
+			<Field type="text" name="username" id="auth-form-username" placeholder="Username" />
+			<ErrorMessage name="username" render={makeLabel.bind(null, "auth-form-username")} />
+		</div>
+		<div>
+			{makeLabel("auth-form-password", "Password")}
+			<Field type="password" name="password" id="auth-form-password" placeholder="Password" />
+			<ErrorMessage name="password" render={makeLabel.bind(null, "auth-form-password")} />
+		</div>
+		<div>
+			<Field type="checkbox" name="rememberMe" id="auth-form-remember-me" />
+			<label htmlFor="auth-form-remember-me">Remember Me</label>
+		</div>
+		<div>{status && status.message}</div>
+		<button type="submit" disabled={isSubmitting}>
+			Submit
+		</button>
+	</Form>
+)
+
+export default ({ action, title }: FormProps) => (
+	<Formik
+		initialValues={{ username: "", password: "", rememberMe: true }}
+		onSubmit={onSubmit.bind(null, action)}
+		render={render.bind(null, title)}
+	/>
+)
