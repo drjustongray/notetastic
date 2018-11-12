9e741262a28dfa21230dca0b9bf3112f975692dd
Justin <drjustongray@gmail.com>
2018-11-11 23:43:37 -0600

    added basic common components

---
+++ webclient/src/common-components/NotFound.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { storiesOf } from '@storybook/react';
+import NotFound from "./NotFound"
+
+const stories = storiesOf("NotFound", module)
+
+stories.add(
+	"Standard",
+	() => <NotFound />
+)
