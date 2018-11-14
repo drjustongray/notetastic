68493c5bf838671fa9472d4b9ca2ee0dc33571f5
Justin <drjustongray@gmail.com>
2018-11-14 00:47:04 -0600

    added AppBar

---
+++ webclient/src/components/AppBar.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import { INDEX, LOGIN, REGISTER, LOGOUT } from "../pages/links";
+import { AuthContext } from "../auth/context";
+import { Subscription } from "rxjs";
+import { AuthService, AuthState } from "../auth/authService";
+import User from "../auth/User";
+import authConnect from "../auth/components/authConnect";
+
+export interface LoggedInBarProps {
+	username: string
+}
+
+export const LoggedInBar = ({ username }: LoggedInBarProps) => (
+	<div>
+		<div>{username}</div>
+		<Link to={LOGOUT}>Log Out</Link>
+	</div>
+)
+
+export const LoggedOutBar = () => (
+	<div>
+		<Link to={LOGIN}>Log In</Link>
+		<Link to={REGISTER}>Register</Link>
+	</div>
+)
+
+interface AppBarButtonsProps {
+	username?: string
+}
+
+function mapAuthStateToProps(authState: AuthState) {
+	return {
+		username: authState.user && authState.user.username
+	}
+}
+
+function renderAppBarButtons({ username }: AppBarButtonsProps) {
+	if (username) {
+		return <LoggedInBar username={username} />
+	}
+	return <LoggedOutBar />
+}
+
+export const AppBarButtons = authConnect(mapAuthStateToProps)<{}>(renderAppBarButtons)
+
+export default () => (
+	<nav>
+		<Link to={INDEX}>Notetastic!</Link>
+		<AppBarButtons />
+	</nav>
+)
