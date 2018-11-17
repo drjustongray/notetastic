a7bf21fc62111b9c8bafa95ba4ef99210aa607f0
Justin <drjustongray@gmail.com>
2018-11-16 18:02:19 -0600

    Note types declared

---
+++ webclient/src/notes/Note.ts
@@ -0,0 +1,27 @@
+export interface Note {
+	Id: string
+	Archived: boolean
+	Title: string
+	Type: string
+}
+
+export interface Bookmark extends Note {
+	Type: "bookmark"
+	URL: string
+}
+
+export interface Checklist extends Note {
+	Type: "checklist"
+	Items: Array<{ Checked: boolean, Text: string }>
+}
+
+export interface Location extends Note {
+	Type: "location"
+	Latitude: number
+	Longitude: number
+}
+
+export interface TextNote extends Note {
+	Type: "text"
+	Text: string
+}
